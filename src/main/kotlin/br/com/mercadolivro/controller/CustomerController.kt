3871fa92db8e7e5d5b11ab63871245d0036ff9c5
Adriel <adrielfreitasalves@gmail.com>
2021-11-02 13:39:53 -0300

    Creating a simple function to create a costumer

--- src/main/kotlin/br/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/br/com/mercadolivro/controller/CustomerController.kt
@@ -1,16 +1,22 @@
 package br.com.mercadolivro.controller
 
 import br.com.mercadolivro.model.CustomerModel
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import br.com.mercadolivro.model.request.PostCustomerRequest
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("customers")
 class CustomerController {
 
     @GetMapping
-    fun helloWorld(): CustomerModel{
+    fun getCostumers(): CustomerModel{
         return CustomerModel("1", "John Doe", "email@email.com")
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody customer:PostCustomerRequest): PostCustomerRequest {
+        return customer
     }
 }
