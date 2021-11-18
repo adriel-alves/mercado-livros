20fc9d08f376f45d688f64b23231153868e02192
Adriel <adrielfreitasalves@gmail.com>
2021-11-18 11:26:51 -0300

    refactoring Controller layer to call Service layer with some simple functions

--- src/main/kotlin/br/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/br/com/mercadolivro/controller/CustomerController.kt
@@ -2,59 +2,40 @@ package br.com.mercadolivro.controller
 
 import br.com.mercadolivro.model.CustomerModel
 import br.com.mercadolivro.model.request.PostCustomerRequest
+import br.com.mercadolivro.service.CustomerService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("customers")
-class CustomerController {
+class CustomerController( val customerService: CustomerService) {
 
-    var customerFakeList = mutableListOf<CustomerModel>()
-
-    @GetMapping()
+    @GetMapping
     fun getCostumers(@RequestParam name: String?): List<CustomerModel> {
-        name?.let {
-            return customerFakeList.filter { it.name.contains(name, true) }
-        }
-
-        return customerFakeList
+        return customerService.getCustomers(name)
     }
 
     @GetMapping("/{id}")
     fun getCostumer(@PathVariable id: String): CustomerModel {
-        return customerFakeList.filter { it.id == id }.first()
+        return customerService.getCustomer(id)
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer: PostCustomerRequest): PostCustomerRequest {
-        createCustomer(customer)
-        return customer
+        return customerService.createCustomer(customer)
     }
 
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun updateCostumer(@PathVariable id: String, @RequestBody customer: PostCustomerRequest) {
-        customerFakeList.filter { it.id == id }.first().let {
-            it.name = customer.name
-            it.email = customer.email
-        }
+        return customerService.updateCustomer(id, customer)
     }
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseStatus(HttpStatus.OK)
     fun deleteCostumer(@PathVariable id: String) {
-        customerFakeList.removeIf { it.id == id }
-    }
-
-    private fun createCustomer(customer: PostCustomerRequest) {
-        val id = if (customerFakeList.isEmpty()) {
-            1
-        } else {
-            customerFakeList.last().id.toInt() + 1
-        }.toString()
-        customerFakeList.add(CustomerModel(id, customer.name, customer.email))
-
+        customerService.deleteCustomer(id)
     }
 }
--- src/main/kotlin/br/com/mercadolivro/service/CustomerService.kt
+++ src/main/kotlin/br/com/mercadolivro/service/CustomerService.kt
@@ -1,7 +1,51 @@
 package br.com.mercadolivro.service
 
+import br.com.mercadolivro.model.CustomerModel
+import br.com.mercadolivro.model.request.PostCustomerRequest
 import org.springframework.stereotype.Service
 
 @Service
 class CustomerService {
+
+    var customerFakeList = mutableListOf<CustomerModel>()
+
+    fun getCustomers(name: String?): List<CustomerModel> {
+        name?.let {
+            return customerFakeList.filter { it.name.contains(name, true)  }
+        }
+        return customerFakeList
+    }
+
+    fun getCustomer(id: String): CustomerModel {
+        return customerFakeList.let { customers ->
+            customers.first {
+                it.id == id
+            }
+        }
+    }
+
+    fun createCustomer(customer: PostCustomerRequest): PostCustomerRequest {
+        val id = if (customerFakeList.isEmpty()) {
+            1
+        } else {
+            customerFakeList.last().id.toInt() + 1
+        }.toString()
+        customerFakeList.add(CustomerModel(id, customer.name, customer.email))
+
+        return customer
+    }
+
+    fun updateCustomer(id: String, customer: PostCustomerRequest) {
+
+        customerFakeList.first { it.id == id }.let {
+            it.name = customer.name
+            it.email = customer.email
+        }
+    }
+
+    fun deleteCustomer(id: String) {
+        customerFakeList.removeIf { it.id == id }
+    }
+
+
 }
