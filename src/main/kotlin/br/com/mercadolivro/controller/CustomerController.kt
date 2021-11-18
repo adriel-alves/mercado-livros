988d3e33c5caeef2393337cf987faa9776d6969c
Adriel <adrielfreitasalves@gmail.com>
2021-11-18 10:13:41 -0300

    adding post put and delete methods to controller manipulating a fake customer list

--- src/main/kotlin/br/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/br/com/mercadolivro/controller/CustomerController.kt
@@ -9,14 +9,52 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("customers")
 class CustomerController {
 
-    @GetMapping
-    fun getCostumers(): CustomerModel{
-        return CustomerModel("1", "John Doe", "email@email.com")
+    var customerFakeList = mutableListOf<CustomerModel>()
+
+    @GetMapping()
+    fun getCostumers(@RequestParam name: String?): List<CustomerModel> {
+        name?.let {
+            return customerFakeList.filter { it.name.contains(name, true) }
+        }
+
+        return customerFakeList
+    }
+
+    @GetMapping("/{id}")
+    fun getCostumer(@PathVariable id: String): CustomerModel {
+        return customerFakeList.filter { it.id == id }.first()
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun create(@RequestBody customer:PostCustomerRequest): PostCustomerRequest {
+    fun create(@RequestBody customer: PostCustomerRequest): PostCustomerRequest {
+        createCustomer(customer)
         return customer
+    }
+
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun updateCostumer(@PathVariable id: String, @RequestBody customer: PostCustomerRequest) {
+        customerFakeList.filter { it.id == id }.first().let {
+            it.name = customer.name
+            it.email = customer.email
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deleteCostumer(@PathVariable id: String) {
+        customerFakeList.removeIf { it.id == id }
+    }
+
+    private fun createCustomer(customer: PostCustomerRequest) {
+        val id = if (customerFakeList.isEmpty()) {
+            1
+        } else {
+            customerFakeList.last().id.toInt() + 1
+        }.toString()
+        customerFakeList.add(CustomerModel(id, customer.name, customer.email))
+
     }
 }
---
+++ src/main/kotlin/br/com/mercadolivro/service/CustomerService.kt
@@ -0,0 +1,7 @@
+package br.com.mercadolivro.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class CustomerService {
+}
